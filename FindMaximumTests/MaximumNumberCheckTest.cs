6e983215e375a5c0c84611d336703e725b2c29a0
Mayank Purohit <mayankpurohit2012@gmail.com>
2020-10-22 11:20:18 +0530

    [Mayank] Add Maximum Integer function

---
+++ FindMaximumTests/MaximumNumberCheckTest.cs
@@ -0,0 +1,26 @@
+﻿using FindMaximum;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FindMaximumTests
+{
+    [TestFixture]
+    class MaximumNumberCheckTest
+    {
+        
+
+        [Test]
+        [TestCase(1,2,3,3)]
+        [TestCase(1, 3, 2,3)]
+        [TestCase(3, 1, 2,3)]
+        public void MaximumIntegerNumber_WhenCalled_ReturnsMaximumInterger(int integer1, int integer2,int integer3, int max)
+        {
+            var result = MaximumNumberCheck.MaximumIntegerNumber(integer1, integer2, integer3);
+
+            Assert.That(result, Is.EqualTo(max));
+        }
+
+    }
+}
