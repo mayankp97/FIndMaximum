d0bae0b4fb0cd82b111cd53ee0d5b8da1acdf4b9
Mayank Purohit <mayankpurohit2012@gmail.com>
2020-10-22 12:12:44 +0530

    [Mayank] Refactor used generic method to find maximum

--- FindMaximumTests/MaximumNumberCheckTest.cs
+++ FindMaximumTests/MaximumNumberCheckTest.cs
@@ -43,5 +43,38 @@ namespace FindMaximumTests
 
             Assert.That(result, Is.EqualTo(max));
         }
+
+        [Test]
+        [TestCase(1, 2, 3, 3)]
+        [TestCase(1, 3, 2, 3)]
+        [TestCase(3, 1, 2, 3)]
+        public void Max_WhenCalled_ReturnsMaximumInterger(int integer1, int integer2, int integer3, int max)
+        {
+            var result = MaximumNumberCheck.Max(integer1, integer2, integer3);
+
+            Assert.That(result, Is.EqualTo(max));
+        }
+
+        [Test]
+        [TestCase(1.1, 2.2, 3.3, 3.3)]
+        [TestCase(1.1, 3.3, 2.2, 3.3)]
+        [TestCase(3.3, 1.1, 2.2, 3.3)]
+        public void Max_WhenCalled_ReturnsMaximumdouble(double double1, double double2, double double3, double max)
+        {
+            var result = MaximumNumberCheck.Max(double1, double2, double3);
+
+            Assert.That(result, Is.EqualTo(max));
+        }
+
+        [Test]
+        [TestCase("Apple", "Banana", "Peach", "Peach")]
+        [TestCase("Peach", "Banana", "Apple", "Peach")]
+        [TestCase("Banana", "Peach", "Apple", "Peach")]
+        public void Max_WhenCalled_ReturnsMax(string string1, string string2, string string3, string max)
+        {
+            var result = MaximumNumberCheck.Max(string1, string2, string3);
+
+            Assert.That(result, Is.EqualTo(max));
+        }
     }
 }
--- MaximumNumberCheck.cs
+++ MaximumNumberCheck.cs
@@ -65,5 +65,22 @@ namespace FindMaximum
 
             throw new Exception("firstNumber,secondNumber and thirdNumber are same");
         }
+
+        public static T Max<T>(T firstValue, T secondValue, T thirdValue) where T : IComparable
+        {
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
+            {
+                return firstValue;
+            }
+            if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
+            {
+                return secondValue;
+            }
+            if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0)
+            {
+                return thirdValue;
+            }
+            throw new Exception("firstNumber,secondNumber and thirdNumber are same");
+        }
     }
 }
